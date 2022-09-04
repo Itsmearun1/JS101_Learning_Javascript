681c60e1a0a21d6c896e2f7b0c321622d5b5bf7f
itsmearun1 <arundevvb1@gmail.com>
2022-09-04 15:28:16 +0530

    logical operators problems

---
+++ web-22-logical-operators/problem4.js
@@ -0,0 +1,14 @@
+//Problem 4: Given 3 numbers (all different values), print which is greatest
+let num1=5;
+let num2=8;
+let num3=1;
+if(num1>num2 && num1>num3){
+  console.log("num1 is greatest");
+}
+else if(num2>num1 && num2>num3){
+  console.log("num2 is greatest");
+}
+else{
+  console.log("num3 is greatest");
+  
+}
---
+++ web-22-logical-operators/problem5.js
@@ -0,0 +1,18 @@
+//Problem 5: Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday", ...
+let day="mon";
+switch(day){
+  case "sun" : console.log("Sunday");
+  break;
+  case "mon" : console.log("Monday");
+  break;
+  case "tue" : console.log("Tuesday");
+  break;
+  case "wed" : console.log("Wednesday");
+  break;
+  case "thu" : console.log("Thursday");
+  break;
+  case "fri" : console.log("Friday");
+  break;
+  case "sat" : console.log("Saturday");
+   
+}
