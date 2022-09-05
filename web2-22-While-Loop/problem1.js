257c904aa3021b78d1cdf098471d7019ad2fe31e
itsmearun1 <arundevvb1@gmail.com>
2022-09-05 22:42:57 +0530

    while loop problems

---
+++ web2-22-While-Loop/problem1.js
@@ -0,0 +1,6 @@
+//Problem 1: Print the numbers from the given starting point till ending point (including both start and end)
+let i=1;
+while (i<=10){
+  console.log(i);
+  i++;
+}
---
+++ web2-22-While-Loop/problem4.js
@@ -0,0 +1,12 @@
+//Problem 4: Print the average of even numbers from 1 to 100 (both included)
+let i=1;
+sum=0;
+count=0;
+while(i<=100){
+  if(i%2==0){
+    sum=sum+i;
+    count++;
+  }
+  i++
+}
+console.log("Average is",sum/count);
